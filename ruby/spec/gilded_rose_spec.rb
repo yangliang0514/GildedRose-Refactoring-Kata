3bda4e1994273e105ef0ca416c983e946ac89ce1
ArturT <arturtrzop@gmail.com>
2015-02-22 15:48:35 +0100

    Add spec for quality of item behaviour

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -49,6 +49,66 @@ describe GildedRose do
       end
     end
 
+    context 'item quality' do
+      it 'quality value is never negative' do
+        item = Item.new('foo', sell_in=0, quality=0)
+        items = [item]
+        gilded_rose = described_class.new(items)
+        gilded_rose.update_quality
+
+        expect(item.quality).to eq 0
+      end
+
+      context 'when sell in date not passed yet' do
+        it 'lowers quality value by 1 at the end of the day' do
+          item = Item.new('foo', sell_in=1, quality=1)
+          items = [item]
+          gilded_rose = described_class.new(items)
+          gilded_rose.update_quality
+
+          expect(item.quality).to eq 0
+        end
+
+        it 'lowers quality value by N after N days' do
+          n = 10
+          item = Item.new('foo', sell_in=n, quality=n)
+          items = [item]
+          gilded_rose = described_class.new(items)
+
+          n.times do
+            gilded_rose.update_quality
+          end
+
+          expect(item.quality).to eq 0
+        end
+      end
+
+      context 'when sell in date has passed' do
+        it 'lowers quality value by 2 at the end of the day' do
+          item = Item.new('foo', sell_in=0, quality=4)
+          items = [item]
+          gilded_rose = described_class.new(items)
+          gilded_rose.update_quality
+
+          expect(item.quality).to eq 2
+        end
+
+        it 'lowers quality value twice as fast after N days' do
+          n = 5
+          quality = 15
+          item = Item.new('foo', sell_in=0, quality=quality)
+          items = [item]
+          gilded_rose = described_class.new(items)
+
+          n.times do
+            gilded_rose.update_quality
+          end
+
+          expect(item.quality).to eq (quality-(2*n))
+        end
+      end
+    end
+
   end
 
 end
