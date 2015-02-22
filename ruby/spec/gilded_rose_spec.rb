b1ce291f207fb1ff0204f67f53efbf3e1a1c1eb5
ArturT <arturtrzop@gmail.com>
2015-02-22 23:48:19 +0100

    Add missing spec to cover default item sell in behaviour

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -5,21 +5,9 @@ require File.join(File.dirname(__FILE__) + '/..', 'gilded_rose_refactored')
 describe GildedRose do
 
   describe '#update_quality' do
-
-    context 'item name' do
-      it 'does not change the name' do
-        item = Item.new('foo', sell_in=0, quality=0)
-        items = [item]
-        gilded_rose = described_class.new(items)
-        gilded_rose.update_quality
-
-        expect(item.name).to eq 'foo'
-      end
-    end
-
-    context 'item sell in' do
+    shared_examples 'default item sell in' do |item_name|
       it 'lowers sell in value by 1 at the end of the day' do
-        item = Item.new('foo', sell_in=1, quality=0)
+        item = Item.new(item_name, sell_in=1, quality=0)
         items = [item]
         gilded_rose = described_class.new(items)
         gilded_rose.update_quality
@@ -29,7 +17,7 @@ describe GildedRose do
 
       it 'lowers sell in value by N after N days' do
         n = 10
-        item = Item.new('foo', sell_in=n, quality=0)
+        item = Item.new(item_name, sell_in=n, quality=0)
         items = [item]
         gilded_rose = described_class.new(items)
 
@@ -42,7 +30,7 @@ describe GildedRose do
       end
 
       it 'sell in value can be negative' do
-        item = Item.new('foo', sell_in=0, quality=0)
+        item = Item.new(item_name, sell_in=0, quality=0)
         items = [item]
         gilded_rose = described_class.new(items)
         gilded_rose.update_quality
@@ -50,6 +38,19 @@ describe GildedRose do
         expect(item.sell_in).to eq -1
       end
     end
+
+    context 'item name' do
+      it 'does not change the name' do
+        item = Item.new('foo', sell_in=0, quality=0)
+        items = [item]
+        gilded_rose = described_class.new(items)
+        gilded_rose.update_quality
+
+        expect(item.name).to eq 'foo'
+      end
+    end
+
+    it_behaves_like 'default item sell in', item_name='foo'
 
     context 'item quality' do
       it 'quality value is never negative' do
@@ -114,6 +115,8 @@ describe GildedRose do
     end
 
     context 'when item is Aged Brie' do
+      it_behaves_like 'default item sell in', item_name='Aged Brie'
+
       context 'item quality' do
         context 'when sell in date not passed yet' do
           it 'increases by 1 the older it gets' do
@@ -187,6 +190,8 @@ describe GildedRose do
     end
 
     context 'when item is Backstage passes to a TAFKAL80ETC concert' do
+      it_behaves_like 'default item sell in', item_name='Backstage passes to a TAFKAL80ETC concert'
+
       context 'item quality' do
         context 'when sell in date not passed yet' do
           context 'when sell in above 10 days' do
@@ -256,6 +261,8 @@ describe GildedRose do
 
     # This is a new feature not yet implemented in GildedRose
     context 'when item is Conjured Mana Cake', :skip do
+      it_behaves_like 'default item sell in', 'Conjured Mana Cake'
+
       context 'item quality' do
         it 'lowers quality value by 2 at the end of the day' do
           item = Item.new('Conjured Mana Cake', sell_in=1, quality=2)
