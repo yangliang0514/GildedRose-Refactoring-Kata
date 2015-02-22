91217f5aa9a75b5962389180194dee3453d2af8d
ArturT <arturtrzop@gmail.com>
2015-02-22 15:31:43 +0100

    Add spec to cover sell in behaviour

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -4,11 +4,51 @@ require File.join(File.dirname(__FILE__) + '/..', 'gilded_rose')
 describe GildedRose do
 
   describe '#update_quality' do
-    it 'does not change the name' do
-      items = [Item.new('foo', 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq 'foo'
+
+    context 'item name' do
+      it 'does not change the name' do
+        item = Item.new('foo', sell_in=0, quality=0)
+        items = [item]
+        gilded_rose = described_class.new(items)
+        gilded_rose.update_quality
+
+        expect(item.name).to eq 'foo'
+      end
     end
+
+    context 'item sell in' do
+      it 'lowers sell in value by 1 at the end of the day' do
+        item = Item.new('foo', sell_in=1, quality=0)
+        items = [item]
+        gilded_rose = described_class.new(items)
+        gilded_rose.update_quality
+
+        expect(item.sell_in).to eq 0
+      end
+
+      it 'lowers sell in value by N after N days' do
+        n = 10
+        item = Item.new('foo', sell_in=n, quality=0)
+        items = [item]
+        gilded_rose = described_class.new(items)
+
+        n.times do
+          gilded_rose.update_quality
+        end
+
+        expect(item.sell_in).to eq 0
+      end
+
+      it 'sell in value can be negative' do
+        item = Item.new('foo', sell_in=0, quality=0)
+        items = [item]
+        gilded_rose = described_class.new(items)
+        gilded_rose.update_quality
+
+        expect(item.sell_in).to eq -1
+      end
+    end
+
   end
 
 end
