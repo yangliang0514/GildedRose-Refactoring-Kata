b81bb990c43dd65f14417fe589b57096d3ee6baf
ArturT <arturtrzop@gmail.com>
2015-02-24 22:53:00 +0100

    Update it for golden master spec

--- ruby/spec/golden_master_spec.rb
+++ ruby/spec/golden_master_spec.rb
@@ -16,7 +16,7 @@ lines_to_compare = [golden_master_lines.count, current_output_lines.count].max
 
 describe 'Golden Master for GildedRose' do
   lines_to_compare.times do |line_num|
-    it "match line #{line_num}: #{current_output_lines[line_num]}" do
+    it "match line #{line_num}: #{current_output_lines[line_num]} should equal #{golden_master_lines[line_num]}" do
       expect(current_output_lines[line_num]).to eq golden_master_lines[line_num]
     end
   end
