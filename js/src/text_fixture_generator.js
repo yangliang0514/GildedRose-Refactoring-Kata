68dfcefa065a6ce7ac4625e69b1e582d430013f3
Hanka <hanka@lunarlogic.io>
2020-02-29 11:56:24 +0100

    (js): extract generating text fixtures

---
+++ js/src/text_fixture_generator.js
@@ -0,0 +1,27 @@
+var lines = [];
+var itemList = [];
+
+itemList.push(new Item('+5 Dexterity Vest', 10, 20));
+itemList.push(new Item('Aged Brie', 2, 0));
+itemList.push(new Item('Elixir of the Mongoose', 5, 7));
+itemList.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
+itemList.push(new Item('Sulfuras, Hand of Ragnaros', -1, 80));
+itemList.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
+itemList.push(new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49));
+itemList.push(new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49));
+// this conjured item does not work properly yet
+// items.push(new Item('Conjured Mana Cake', 3, 6));
+
+function generate_fixtures(days, items) {
+  for(var day = 0; day <= days; day++) {
+    lines.push('-------- day ' + day + ' --------')
+
+    for(var i = 0; i < itemList.length; i++) {
+      var item = items[i];
+      lines.push(`${item.name}, ${item.sell_in}, ${item.quality}`)
+    }
+
+    lines.push('<br />');
+    update_quality(items);
+  }
+}
