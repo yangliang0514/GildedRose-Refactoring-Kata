5008a53b583ef204814fdec221e26eb06da623ba
Kevin Yang <e510021@gmail.com>
2024-10-14 18:20:54 +0800

    Refactor nested if else into by item names

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,55 +1,51 @@
 class GildedRose
 
+  @@item_names = ["Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"]
+
   def initialize(items)
     @items = items
   end
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
+      if item.name == "Aged Brie"
+        item.quality = item.quality + 1 if item.quality < 50
+        item.sell_in -= 1
+
+        if item.sell_in < 0 and item.quality < 50
+          item.quality += 1
         end
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
+      
+      if item.name == "Backstage passes to a TAFKAL80ETC concert"
+        item.quality += 1 if item.quality < 50
+        item.quality += 1 if item.sell_in < 11 and item.quality < 50
+        item.quality += 1 if item.sell_in < 6 and item.quality < 50
+        item.sell_in -= 1
+
+        if item.sell_in < 0
+          item.quality = 0
+        end
       end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
+
+      if item.name == "Sulfuras, Hand of Ragnaros"
+      end
+
+      if is_others?(item.name)
+        item.quality = item.quality > 0 ? item.quality - 1 : item.quality
+        item.sell_in -= 1
+
+        if item.sell_in < 0 and item.quality > 0
+          item.quality -= 1
         end
       end
     end
+  end
+  
+  private
+
+  def is_others?(item_name)
+    not @@item_names.include?(item_name)
   end
 end
 
