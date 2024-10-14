61cc84e34dba80c0e19081b69f9711b2ea46b7db
Kevin Yang <e510021@gmail.com>
2024-10-14 22:18:43 +0800

    Extract update logic into helper classes

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,109 +1,18 @@
 class GildedRose
-
-  @@special_items = ["Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"]
-
   def initialize(items)
+    @@item_updaters = {
+      "Aged Brie" => AgedBrieUpdater, 
+      "Backstage passes to a TAFKAL80ETC concert" => BackstagePassUpdater, 
+      "Sulfuras, Hand of Ragnaros" => SulfurasUpdater
+    }
     @items = items
   end
 
-  # specs:
-  #  "Aged Brie":
-  #   1. Decrease `sell_in` by 1 each day (on each update).
-  #   2. After `sell_in` is decreased:
-  #      - Increase `quality` by 1 if `sell_in` >= 0.
-  #      - Increase `quality` by 2 if `sell_in` < 0.
-  #   
-  #  "Backstage passes to a TAFKAL80ETC concert":
-  #   1. Decrease `sell_in` by 1 each day (on each update).
-  #   2. After `sell_in` is decreased:
-  #      - If `sell_in` >= 10: increase `quality` by 1.
-  #      - If 5 <= `sell_in` < 10: increase `quality` by 2.
-  #      - If 0 <= `sell_in` < 5: increase `quality` by 3.
-  #      - If `sell_in` < 0: reset `quality` to 0.
-  #     
-  #  "Sulfuras, Hand of Ragnaros":
-  #   1. No operations on `sell_in` or `quality`.
-  #   
-  #  Other items:
-  #   1. Decrease `sell_in` by 1 each day (on each update).
-  #   2. After `sell_in` is decreased:
-  #      - Decrease `quality` by 1 if `sell_in` >= 0.
-  #      - Decrease `quality` by 2 if `sell_in` < 0.
-
   def update_quality()
     @items.each do |item|
-      if item.name == "Aged Brie"
-        decrease_sell_in(item)
-
-        case
-        when item.sell_in >= 0
-          increase_quality(item)
-        when item.sell_in < 0
-          increase_quality(item, times: 2)
-        end
-      end
-      
-      if item.name == "Backstage passes to a TAFKAL80ETC concert"
-        decrease_sell_in(item)
-
-        case 
-        when item.sell_in >= 10
-          increase_quality(item)
-        when item.sell_in < 10 && item.sell_in >= 5
-          increase_quality(item, times: 2)
-        when item.sell_in < 5 && item.sell_in >= 0
-          increase_quality(item, times: 3)
-        when item.sell_in < 0
-          reset_quality(item)
-        end
-      end
-
-      if item.name == "Sulfuras, Hand of Ragnaros"
-      end
-
-      if is_others?(item.name)
-        decrease_sell_in(item)
-
-        case
-        when item.sell_in >= 0
-          decrease_quality(item)
-        when item.sell_in < 0
-          decrease_quality(item, times: 2)
-        end
-      end
+      updater = @@item_updaters[item.name] || DefaultItemUpdater
+      updater.update(item)
     end
-  end
-  
-  private
-
-  def is_others?(item_name)
-    !@@special_items.include?(item_name)
-  end
-
-  def increase_quality(item, options = {})
-    options = {times: 1}.merge(options)
-
-    options[:times].times do
-      return if item.quality >= 50
-      item.quality += 1
-    end
-  end
-
-  def decrease_quality(item, options = {})
-    options = {times: 1}.merge(options)
-
-    options[:times].times do
-      return if item.quality <= 0
-      item.quality -= 1
-    end
-  end
-
-  def decrease_sell_in(item)
-    item.sell_in -= 1
-  end
-
-  def reset_quality(item)
-    item.quality = 0
   end
 end
 
@@ -118,5 +27,109 @@ class Item
 
   def to_s()
     "#{@name}, #{@sell_in}, #{@quality}"
+  end
+end
+
+# specs:
+#  `quality` ranges from 0 to 50.
+#
+#  "Aged Brie":
+#   1. Decrease `sell_in` by 1 each day (on each update).
+#   2. After `sell_in` is decreased:
+#      - Increase `quality` by 1 if `sell_in` >= 0.
+#      - Increase `quality` by 2 if `sell_in` < 0.
+#   
+#  "Backstage passes to a TAFKAL80ETC concert":
+#   1. Decrease `sell_in` by 1 each day (on each update).
+#   2. After `sell_in` is decreased:
+#      - If `sell_in` >= 10: increase `quality` by 1.
+#      - If 5 <= `sell_in` < 10: increase `quality` by 2.
+#      - If 0 <= `sell_in` < 5: increase `quality` by 3.
+#      - If `sell_in` < 0: reset `quality` to 0.
+#     
+#  "Sulfuras, Hand of Ragnaros":
+#   1. No operations on `sell_in` or `quality`.
+#   
+#  Other items:
+#   1. Decrease `sell_in` by 1 each day (on each update).
+#   2. After `sell_in` is decreased:
+#      - Decrease `quality` by 1 if `sell_in` >= 0.
+#      - Decrease `quality` by 2 if `sell_in` < 0.
+
+class DefaultItemUpdater
+
+  def self.update(item)
+    decrease_sell_in(item)
+    decrease_quality(item, times: calc_times(item.sell_in))
+  end
+  
+  protected
+
+  def self.calc_times(sell_in)
+    return 1 if sell_in >= 0
+    2
+  end
+
+  def self.increase_quality(item, options = {})
+    options = {times: 1}.merge(options)
+
+    options[:times].times do
+      return if item.quality >= 50
+      item.quality += 1
+    end
+  end
+
+  def self.decrease_quality(item, options = {})
+    options = {times: 1}.merge(options)
+
+    options[:times].times do
+      return if item.quality <= 0
+      item.quality -= 1
+    end
+  end
+
+  def self.decrease_sell_in(item, options = {})
+    options = {times: 1}.merge(options)
+
+    options[:times].times do
+      item.sell_in -= 1
+    end
+  end
+
+  def self.reset_quality(item)
+    item.quality = 0
+  end
+end
+
+class AgedBrieUpdater < DefaultItemUpdater
+  def self.update(item)
+    decrease_sell_in(item)
+    increase_quality(item, times: calc_times(item.sell_in))
+  end
+end
+
+class BackstagePassUpdater < DefaultItemUpdater
+  def self.update(item)
+    decrease_sell_in(item)
+    return reset_quality(item) if item.sell_in < 0
+    increase_quality(item, times: calc_times(item.sell_in))
+  end
+
+  protected
+
+  def self.calc_times(sell_in)
+    case sell_in
+    when 10..Float::INFINITY
+      1
+    when 5..9
+      2
+    when 0..4
+      3
+    end
+  end
+end
+
+class SulfurasUpdater < DefaultItemUpdater
+  def self.update(_item)
   end
 end
